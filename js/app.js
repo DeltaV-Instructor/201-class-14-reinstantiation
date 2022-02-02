f001d56ffc152d868e77e3fe2054b359e6f92075
tektechnologies <info@tektechnologies.com>
2022-02-01 19:00:26 -0600

    add constructor and event listener

--- js/app.js
+++ js/app.js
@@ -4,3 +4,28 @@ console.log('app js connected');
 let allPizzas = [];
 let pizzaForm = document.getElementById('pizzaForm');
 let pizzaList = document.getElementById('pizzaList');
+console.log({allPizzas, pizzaForm, pizzaList});
+
+function Pizza(pizzaType){
+  this.pizzaType = pizzaType;
+
+  this.render = function(){
+    const listItem = document.createElement('li');
+    listItem.textContent = this.pizzaType;
+    pizzaList.appendChild(listItem);
+  },
+  allPizzas.push(this);
+}
+
+
+function handlePizzaSubmit(event){
+  event.preventDefault();
+
+  let newPizza = new Pizza(event.target.pizzaInput.value);
+  pizzaForm.reset();
+  newPizza.render();
+
+
+}
+
+
--- js/appOne.js
+++ js/appOne.js
@@ -1,2 +1,24 @@
 'use strict';
 console.log('app one js connected');
+
+
+//grab local storage and render to page
+
+
+
+
+
+
+
+
+
+
+
+
+//add our event listener for our app.js
+pizzaForm.addEventListener('submit', handlePizzaSubmit);
+
+
+
+
+
