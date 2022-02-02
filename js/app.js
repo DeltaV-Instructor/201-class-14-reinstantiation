e8a66d0ed5029fd9c7bd922c6720c3fa5a1184de
tektechnologies <info@tektechnologies.com>
2022-02-01 19:29:19 -0600

    add local storage render to second page.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ console.log('app js connected');
 let allPizzas = [];
 let pizzaForm = document.getElementById('pizzaForm');
 let pizzaList = document.getElementById('pizzaList');
-console.log({allPizzas, pizzaForm, pizzaList});
+// console.log({allPizzas, pizzaForm, pizzaList});
 
 function Pizza(pizzaType){
   this.pizzaType = pizzaType;
@@ -24,6 +24,17 @@ function handlePizzaSubmit(event){
   let newPizza = new Pizza(event.target.pizzaInput.value);
   pizzaForm.reset();
   newPizza.render();
+
+  //one way 
+  // const arrayString = JSON.stringify(allPizzas);
+  // console.log(arrayString);
+  // localStorage.setItem('pizzas', arrayString);
+  //way two 
+  // localStorage.setItem('pizzas', JSON.stringify(allPizzas));
+
+  //last 
+  localStorage.pizzas = JSON.stringify(allPizzas);
+  console.log(localStorage.pizzas);
 
 
 }
