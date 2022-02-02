f8642e08c89530f1f075dd6ff0023662c85ee96d
tektechnologies <info@tektechnologies.com>
2022-02-01 18:49:01 -0600

    add basic css and js consolelog

---
+++ js/app.js
@@ -0,0 +1,6 @@
+'use strict';
+console.log('app js connected');
+
+let allPizzas = [];
+let pizzaForm = document.getElementById('pizzaForm');
+let pizzaList = document.getElementById('pizzaList');
---
+++ js/appOne.js
@@ -0,0 +1,2 @@
+'use strict';
+console.log('app one js connected');
